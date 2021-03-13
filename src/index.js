c451c93209bdc4257889d6c95334e5dafdf31e97
Jamari0 <jgjsguilherme@gmail.com>
2021-03-13 15:28:40 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const { response, request } = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = []
+
+function verifyIfExistaccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found " });
+    }
+    request.customer = customer
+    return next();
+}
+
+app.post("/account",  (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customersAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf);
+
+    if (customersAlreadyExists) {
+        return response.status(400).json({ error: "customer already exists!" });
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+
+
+});
+
+app.get("/statement", verifyIfExistaccountCPF, (request, response) => {
+    const {customer} = request;
+
+    return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistaccountCPF, (request,response) => {
+    const {description, amount} = request.body;
+    const {customer} = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+app.listen(3333);
